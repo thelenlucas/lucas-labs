72dfcff213549c053659ebd0d0bb4190e9945da0
Lucas Thelen <thelenlucas028@gmail.com>
2024-09-22 16:07:43 -0500

    trapezoid

--- lab-two/shapes.s
+++ lab-two/shapes.s
@@ -30,6 +30,11 @@ loop:
 	cmp	r0, #3
 	beq loop_rect
 
+	@ Trapizoid
+	cmp	r0, #4
+	beq loop_trap
+
+
 loop_triangle:
 	@ Get base
 	LDR	r0, =base_string
@@ -71,6 +76,7 @@ loop_square:
 	@ Hijack rectangle_area for square
 	LDR	r0, =int_a
 	LDR	r0, [r0]
+	mov r1, r0
 	push {r0, r1}
 	BL	rectangle_area
 
@@ -96,6 +102,45 @@ loop_rect:
 	LDR	r1, [r1]
 	push {r0, r1}
 	BL	rectangle_area
+
+	@ Print result
+	POP {r1}
+	LDR	r0, =print_area
+	BL	printf
+
+	B	loop_end
+
+loop_trap:
+	@ Get base
+	LDR	r0, =base_string
+	BL	printf
+	LDR	r0, =format_int
+	LDR	r1, =int_a
+	BL	scanf
+
+	@ Get upper base
+	LDR	r0, =upper_base_string
+	BL	printf
+	LDR	r0, =format_int
+	LDR	r1, =int_b
+	BL	scanf
+
+	@ Get height
+	LDR	r0, =height_string
+	BL	printf
+	LDR	r0, =format_int
+	LDR	r1, =int_c
+	BL	scanf
+
+	@ Pass base, upper base, and height to trapizoid_area
+	LDR	r0, =int_a
+	LDR	r1, =int_b
+	LDR	r2, =int_c
+	LDR	r0, [r0]
+	LDR	r1, [r1]
+	LDR	r2, [r2]
+	push {r0, r1, r2}
+	BL	trapizoid_area
 
 	@ Print result
 	POP {r1}
@@ -175,6 +220,49 @@ rect_calc_end:
 	push {r0}
 	bx lr
 
+@ Trapizoid area calculation, a bit more complex
+@ Have to check for overflow both after add and multiply
+@ Also has a third input, height, but otherwise similar
+trapizoid_area:
+	@ Inputs are on stack, pop them to r10, r11, r12
+	pop {r10, r11, r12}
+
+	@ Preserve lr
+	push {lr}
+
+	@ Calculate area
+	@ First, add the bases
+	add r0, r10, r11
+	bvs trap_add_overflow
+	b trap_mul
+
+trap_add_overflow:
+	bl overflow
+	b trap_calc_end
+
+trap_mul:
+	smull r0, r1, r0, r12
+
+	@ Check for overflow,
+	cmp r1, #0
+	bne trap_calc_overflow
+
+	b trap_calc_end
+
+trap_calc_overflow:
+	bl overflow
+	b trap_calc_end
+
+trap_calc_end:
+	@ Divide by 2, using asr
+	asr r0, r0, #1
+
+	@ Get link back
+	pop {lr}
+	@ Push result to stack
+	push {r0}
+	bx lr
+
 exit:
 	mov	r7, #1
 	svc	0
@@ -189,6 +277,7 @@ base_string: .asciz "Please enter a base length: "
 .balign 4
 height_string: .asciz "Please enter a height length: "
 .balign 4
+upper_base_string: .asciz "Please enter the upper base length: "
 overflow_result: .asciz "Overflow detected\n"
 .balign 4
 print_area: .asciz "The area is: %d!\n"
