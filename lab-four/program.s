4196fc863d39553e2d9ab9e84b05820c100a7a65
Lucas Thelen <thelenlucas028@gmail.com>
2024-10-25 17:47:16 -0500

    Don't think we need unified

--- lab-four/program.s
+++ lab-four/program.s
@@ -6,32 +6,33 @@
     @ PURPOSE: Prints a welcome message and exits.
     @
     @ COMPILE AND RUN: 
-    @ 1. as -o source.o source.s && gcc -o program source.o
+    @ 1. as -mthumb -o source.o source.s && gcc -mthumb -o program source.o
     @ 2. ./program
     @ ------------------------------------------------------------
 
     .global main
     .global printf
-    .syntax unified        @ Use unified syntax for ARM/Thumb compatibility
-    .thumb                 @ Assemble in Thumb mode
-    .arch armv6            @ Specify ARMv6 architecture
+    .thumb				@ Assemble in Thumb mode
+    .arch armv6			@ Specify ARMv6 architecture
 
-    .text                  @ Code section
+    .text				@ Code section
 
-    .thumb_func            @ Indicate that the following is a Thumb function
+    .thumb_func	@ Indicate that the following is a Thumb function, we won't leave so we don't need to re-identify
     main:
         @ Save the link register (return address)
         push {lr}
 
         @ Load the address of the welcome message into r0 (first argument to printf)
         ldr r0, =welcome
-        bl printf          @ Call printf
+        bl printf	@ Call printf
 
         @ Set the return value to 0 (success)
         movs r0, #0
 
         @ Restore the link register and return from main
-        pop {pc}           @ In Thumb mode, popping pc is valid for returning
+        pop {pc}	@ In Thumb mode, popping pc is valid for returning
+
+	.ltorg	@ Force the assembler to stick literals here, so they're still accessable later
 
     @ Data section
     .data
